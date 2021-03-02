816efdb032c7b2556ae381e043daa1dd8f77fa15
Luis Javier Medina Coca <luismedinacoca@gmail.com>
2021-03-02 07:24:59 -0300

    Add files via upload

---
+++ test-output-thread/report.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "bdfca294-b198-4abd-90a6-29bf83c24af2",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1614597173664,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614597182063,
+    "className": "failed"
+  },
+  {
+    "id": "9609d0d8-7ea0-4ee5-b258-a3198ed19ea1",
+    "feature": "Account Page Feature",
+    "scenario": "Account section count",
+    "start": 1614597204416,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614597216531,
+    "className": "passed"
+  },
+  {
+    "id": "37863cf2-7cb4-4589-a28f-88c1d032c7d6",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1614597182083,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614597192934,
+    "className": "passed"
+  },
+  {
+    "id": "8ca069a2-2217-48b0-a829-03d1ece26b1b",
+    "feature": "Account Page Feature",
+    "scenario": "Account page title",
+    "start": 1614597192946,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614597204407,
+    "className": "passed"
+  },
+  {
+    "id": "5635c3e3-ecd2-4cbe-938a-30245be142dc",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1614597165422,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1614597173651,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
